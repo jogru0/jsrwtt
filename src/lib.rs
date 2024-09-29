ce92f82610ddcfcc0ea7d281b727daae50c2df0d
Jonathan Gruner <x432ph@gmx.de>
2024-09-29 14:16:55 +0200

    logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use cgmath::prelude::*;
+use log::info;
 use pollster::FutureExt;
 use state::State;
 use winit::{
@@ -221,6 +222,7 @@ impl StateApplication {
 
 impl ApplicationHandler for StateApplication {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        info!("Application resumed.");
         let window = event_loop
             .create_window(Window::default_attributes().with_title(&self.title))
             .unwrap();
@@ -297,33 +299,18 @@ pub async fn run() {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
             console_log::init_with_level(log::Level::Info).expect("Could't initialize logger");
         } else {
-            env_logger::init();
+            env_logger::Builder::new()
+                .filter_level(log::LevelFilter::Warn)
+                .filter_module("jsrwtt", log::LevelFilter::Info)
+                .filter_module("wgpu_hal::vulkan::instance", log::LevelFilter::Error)
+                .init();
         }
     }
 
     let event_loop = EventLoop::new().unwrap();
 
-    let title = env!("CARGO_PKG_NAME").into();
-
-    #[cfg(target_arch = "wasm32")]
-    {
-        // Winit prevents sizing with CSS, so we have to set
-        // the size manually when on web.
-        use winit::dpi::PhysicalSize;
-        let _ = window.request_inner_size(PhysicalSize::new(450, 400));
-
-        use winit::platform::web::WindowExtWebSys;
-        web_sys::window()
-            .and_then(|win| win.document())
-            .and_then(|doc| {
-                let dst = doc.get_element_by_id("wasm-example")?;
-                let canvas = web_sys::Element::from(window.canvas()?);
-                dst.append_child(&canvas).ok()?;
-                Some(())
-            })
-            .expect("Couldn't append canvas to document body.");
-    }
+    let title = "Jonathan's Sausage Roll With Time Travel".into();
 
     let mut window_state = StateApplication::new(title);
-    let _ = event_loop.run_app(&mut window_state);
+    event_loop.run_app(&mut window_state).unwrap();
 }
